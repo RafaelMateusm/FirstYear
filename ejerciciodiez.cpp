ab010005067886362fa7cc6fe8d1abd2b031231b
root <root@localhost.localdomain>
2018-10-04 02:38:45 -0500

    Simplificación en funciones.

--- ejerciciodiez.cpp
+++ ejerciciodiez.cpp
@@ -2,24 +2,33 @@
 #include <cmath>
 using namespace std;
 
-int main() {
-  //Declaramos las variables.
   double lado1, lado2, angulogrados, anguloradianes, area;
 
+  int pedirdatos(){   //Obtenemos los datos del usuario y los guardamos en las variables declaradas.
+    cout << "Introduce el primer lado:";
+    cin >> lado1;
+    cout << "Introduce el segundo lado:";
+    cin >> lado2;
+    cout << "Introduce el ángulo: ";
+    cin >> angulogrados;
+    return 0;
+  }
+  int calcularangulo(){
+      anguloradianes = (angulogrados * M_PI) /180;
+      return 0;
+  }
+  int calculararea(){
+      area = 0.5 * lado1 * lado2 * sin(anguloradianes);
+      return 0;
+  }
+int main() {
   //Obtenemos los datos del usuario y los guardamos en las variables declaradas.
-  cout << "Introduce el primer lado:";
-  cin >> lado1;
-  cout << "Introduce el segundo lado:";
-  cin >> lado2;
-  cout << "Introduce el ángulo: ";
-  cin >> angulogrados;
-
+  pedirdatos();
   //Calculamos el ángulo en radianes.
-  anguloradianes = (angulogrados * M_PI) /180;
-
+  cout << lado1;
+  calcularangulo();
   //Calculamos el área.
-  area = 0.5 * lado1 * lado2 * sin(anguloradianes);
-
+  calculararea();
   //Imprime el resultado.
   cout << "El área del triángulo es: " << area;
 
